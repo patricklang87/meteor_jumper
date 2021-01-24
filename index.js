ba16fca5fffc44fa59eab389c0aceff393ab16d7
Patrick <patricklang87@gmail.com>
2021-01-24 16:54:24 -0500

    add styling and moving asteroids

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ class Field {
         let fieldVisual = document.createElement('div');
         fieldVisual.style.width = width*50 + "px";
         fieldVisual.style.height = height*50 + "px";
-        fieldVisual.style.backgroundImage = "url('https://media.giphy.com/media/cjojgtiigyloyCg3r9/source.gif')";
+        //fieldVisual.style.backgroundImage = "url('https://media.giphy.com/media/cjojgtiigyloyCg3r9/source.gif')";
         fieldVisual.style.borderRadius = "10px";
         fieldVisual.style.position = "relative";
         fieldVisual.id = "fieldVisual";
@@ -66,10 +66,14 @@ class Field {
         for (let y = 0; y <this.field.length; y++) {
             for (let x = 0; x < this.field[0].length; x++) {
                 if (this.field[y][x] == hole) {
+                    let asteroidImage = document.createElement('img');
+                    asteroidImage.src = "resources/images/asteroid.gif";
+                    asteroidImage.style.height = "50px";
+                    asteroidImage.style.width = "50px";
                     let holeDiv = document.createElement('div');
                     holeDiv.style.height = "50px";
                     holeDiv.style.width = "50px";
-                    holeDiv.style.backgroundColor = "black";
+                    holeDiv.appendChild(asteroidImage);
                     holeDiv.style.position = "absolute";
                     holeDiv.style.top = y*50 + "px";
                     holeDiv.style.left = x*50 + "px";
@@ -79,10 +83,15 @@ class Field {
                 }
 
                 if (this.field[y][x] == hat) {
+                    let issImage = document.createElement('img');
+                    issImage.src = "resources/images/astronaut.gif";
+                    issImage.style.maxHeight = "50px";
+                    issImage.style.maxWidth = "50px";
+                    issImage.style.transform = "scaleY(-1)";
                     let holeDiv = document.createElement('div');
                     holeDiv.style.height = "50px";
                     holeDiv.style.width = "50px";
-                    holeDiv.style.backgroundColor = "blue";
+                    holeDiv.appendChild(issImage);
                     holeDiv.style.position = "absolute";
                     holeDiv.style.top = y*50 + "px";
                     holeDiv.style.left = x*50 + "px";
@@ -92,10 +101,15 @@ class Field {
                 }
 
                 if (this.field[y][x] == man) {
+                    let ufoImage = document.createElement('img');
+                    ufoImage.src = "resources/images/ufo.gif";
+                    ufoImage.id = "ufo";
+                    ufoImage.style.height = "50px";
+                    ufoImage.style.width = "50px";
                     let holeDiv = document.createElement('div');
-                    holeDiv.style.height = "50px";
-                    holeDiv.style.width = "50px";
-                    holeDiv.style.backgroundColor = "red";
+                    holeDiv.style.height = "70px";
+                    holeDiv.style.width = "70px";
+                    holeDiv.appendChild(ufoImage);
                     holeDiv.style.position = "absolute";
                     holeDiv.style.top = y*50 + "px";
                     holeDiv.style.left = x*50 + "px";
@@ -125,6 +139,8 @@ class Field {
         document.getElementById("reset").style.display = "block";
     }
 
+ 
+
     static up() {
         let manPos = document.getElementById("man").style.top;
         let newPos = Number(manPos.substring(0, manPos.length - 2));
@@ -147,6 +163,10 @@ class Field {
         Field.checkPos();
     }
 
+    static hold() {
+        Field.checkPos();
+    }
+
     static right() {
         let manPos = document.getElementById("man").style.left;
         let newPos = Number(manPos.substring(0, manPos.length - 2));
@@ -158,7 +178,7 @@ class Field {
         Field.checkPos();
     }
 
-    static down() {
+    static jump() {
         let manPos = document.getElementById("man").style.top;
         let newPos = Number(manPos.substring(0, manPos.length - 2));
         newPos += 50;
@@ -169,21 +189,8 @@ class Field {
         Field.checkPos();
     }
 
-    static checkPos() {
-        let manPosY = document.getElementById("man").style.top;
-        let manPosX = document.getElementById("man").style.left;
-        let hatPosY = document.getElementById("hat").style.top;
-        let hatPosX = document.getElementById("hat").style.left;
-        let fieldHeight = document.getElementById("fieldVisual").style.height;
-        let fieldWidth = document.getElementById("fieldVisual").style.width;
+    static determineHoleCoords() {
         let holes = document.getElementsByClassName("hole");
-        
-        manPosY = Number(manPosY.substring(0, manPosY.length - 2));
-        manPosX = Number(manPosX.substring(0, manPosX.length - 2));
-        hatPosY = Number(hatPosY.substring(0, hatPosY.length - 2));
-        hatPosX = Number(hatPosX.substring(0, hatPosX.length - 2));
-        fieldHeight = Number(fieldHeight.substring(0, fieldHeight.length - 2));
-        fieldWidth = Number(fieldWidth.substring(0, fieldWidth.length - 2));
         let holeArrayXY = [];
         for (let hole = 0; hole < holes.length; hole++) {
             let holeCoords = [];
@@ -194,39 +201,71 @@ class Field {
             holeCoords.push(holePosX, holePosY);
             holeArrayXY.push(holeCoords);
         }
+        return holeArrayXY;
+    }
+
+    static updateHoleCoords() {
+        let fieldVisual = document.getElementById("fieldVisual"); 
+        let fieldVisChildren = fieldVisual.children;
+        for (let i = 0; i < fieldVisChildren.length; i++) {
+            let child = fieldVisChildren[i];
+            console.log("chilren: ", child.classList);
+            if (child.classList[0] == "hole") {
+                let holePosY = child.style.top;
+                holePosY = Number(holePosY.substring(0, holePosY.length-2));
+                holePosY -= 50;
+                if (holePosY < 0) {
+                    console.log("holePosY too low: ", holePosY);
+                    let fieldVisHeight = fieldVisual.style.height;
+                    fieldVisHeight = Number(fieldVisHeight.substring(0, fieldVisHeight.length-2));
+                    fieldVisHeight -= 50;
+                    holePosY = fieldVisHeight;
+                    console.log("new holePosY: ", holePosY);
+                }
+                let newHolePosY = `${holePosY}px`;
+                child.style.top = newHolePosY;
+            }
+        }
+        
+    }
+
+    static checkPos() {
+        Field.updateHoleCoords();
+        let manPosY = document.getElementById("man").style.top;
+        let manPosX = document.getElementById("man").style.left;
+        let hatPosY = document.getElementById("hat").style.top;
+        let hatPosX = document.getElementById("hat").style.left;
+        let fieldHeight = document.getElementById("fieldVisual").style.height;
+        let fieldWidth = document.getElementById("fieldVisual").style.width;
+            
+        manPosY = Number(manPosY.substring(0, manPosY.length - 2));
+        manPosX = Number(manPosX.substring(0, manPosX.length - 2));
+        hatPosY = Number(hatPosY.substring(0, hatPosY.length - 2));
+        hatPosX = Number(hatPosX.substring(0, hatPosX.length - 2));
+        fieldHeight = Number(fieldHeight.substring(0, fieldHeight.length - 2));
+        fieldWidth = Number(fieldWidth.substring(0, fieldWidth.length - 2));
+        let holeArrayXY = Field.determineHoleCoords();
+
         console.log("hole coords", holeArrayXY[0]);
         if (manPosY == hatPosY && manPosX == hatPosX) {
-            document.getElementById("announcementDiv").innerText = "Congratulations! You found your hat!";
+            document.getElementById("announcementDiv").innerText = "Congratulations! Delicious earthling!";
             Field.endGame();
         }
         if (manPosY < 0 || manPosX < 0 || manPosY >= fieldHeight || manPosX >= fieldWidth) {
-            document.getElementById("announcementDiv").innerText = "You've fallen off of the plane of existence and evaporated into the ether!";
+            document.getElementById("announcementDiv").innerText = "You've been swept into the comic undertow torn to pieces!";
             Field.endGame();
         }
         for (let XYpair = 0; XYpair < holeArrayXY.length; XYpair++) {
             let holePosX = holeArrayXY[XYpair][0];
             let holePosY = holeArrayXY[XYpair][1];
             if (manPosX == holePosX && manPosY == holePosY) {
-                document.getElementById("announcementDiv").innerText = "You fell down a deep, deep hole and broke all your bones!";
+                document.getElementById("announcementDiv").innerText = "You smashed into an asteroid and broke all your bones!";    
+                document.getElementById("ufo").src = "resources/images/explosion1.gif";
                 Field.endGame();
             }
         }
     }
 
-    /*  
-    static moveMan(event) {
-        let x = event.keycode;
-        if (x == 40) {
-            let manPos = document.getElementById("man").style.top;
-            let newPos = Number(manPos.substring(0, manPos.length - 2));
-            newPos += 50;
-            console.log(newPos);
-            newPos = newPos.toString() +"px";
-            console.log(newPos);
-            document.getElementById("man").style.top = newPos;
-        }
-    }
-    */
 
     static startGame() {
         let gameField = new Field(Field.fieldFromInput());
@@ -236,7 +275,7 @@ class Field {
         document.getElementById("buttonDiv").style.display = "block";
         document.getElementById("setupDiv").style.display = "none";
         document.getElementById("announcementDiv").style.display = "block";
-        document.getElementById("announcementDiv").textContent = "You dropped your hat. You need to find your hat!";
+        document.getElementById("announcementDiv").textContent = "Find and devour the devour the foolish earthling!";
     }
     
 }
@@ -248,7 +287,8 @@ class Field {
 document.getElementById("start").addEventListener("click", Field.startGame);
 document.getElementById("up").addEventListener("click", Field.up);
 document.getElementById("right").addEventListener("click", Field.right);
+document.getElementById("hold").addEventListener("click", Field.hold);
 document.getElementById("left").addEventListener("click", Field.left);
-document.getElementById("down").addEventListener("click", Field.down);
+document.getElementById("jump").addEventListener("click", Field.jump);
 document.getElementById("reset").addEventListener("click", Field.reset);
 //window.addEventListener("keydown", Field.moveMan);
